bf17b217618537b8f35915d2a3b680120063b269
labschedule <151550805+labschedule@users.noreply.github.com>
2023-11-26 11:30:59 +0200

    Update update_r.sh

--- scripts/update_r.sh
+++ scripts/update_r.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Get the script's directory, regardless of where the script is executed from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change the current working directory to the script's directory
+cd "$SCRIPT_DIR"
+
 # pull files every hour
 
 while :
